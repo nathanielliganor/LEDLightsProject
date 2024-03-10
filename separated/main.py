35d36ebeaf03b8b34c6abc0d0420cceebfbcc675
Nicholas Ketchum <nketchum@gmail.com>
2024-03-10 19:03:08 -0400

    add flag as req 7

--- separated/main.py
+++ separated/main.py
@@ -1,17 +1,18 @@
 from _send_signals import send_signals
 from _get_blank_sequence import getBlankSequence
+
 from _req1_color_sequence import color_sequence
 from _req2_maize_blue import get_umich_lights
 from _req3_alternate_lights import alternate_lights
 from _req4_any_sequence import danceToBeats
 from _req5_chase import chase
 # from _req6_clock import hour_tracker
+# from _req7_extra_flag import generate_flag_signals
 
 
 def main():
     # Execute each requirement by commenting out
     # the requirement you'd like to run.
-    
     color_sequence()
     # get_umich_lights()
     # alternate_lights()
@@ -21,7 +22,7 @@ def main():
     #       '255,100,255,100',
     #       3, 1, 0.0001, False),
     # hour_tracker() # Uncomment out from/import above to active
-
-
+    # generate_flag_signals()
+g
 if __name__ == "__main__":
     main()
